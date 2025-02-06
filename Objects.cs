d465a7a46449dcee8db8917c4bb175626889f7f0
Artyom Tarasov <supur2014@yandex.ru>
2025-02-06 03:05:46 +0300

    first commit

---
+++ Objects.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_c_
+{
+    public class Objects
+    {
+        public struct Point2D
+        {
+            public double x { get; } 
+            public double y { get; }
+
+            public Point2D(double x, double y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+
+            public double GetX => this.x;
+            public double GetY => this.y;
+
+            public double Distance(Point2D other)
+            {
+                return Math.Sqrt((x - other.x) * (x - other.x) + (y - other.y) * (y - other.y));
+            }
+        }
+
+        public interface IChecker
+        {
+            bool Check(Point2D other, Triangle triangle);
+        }
+        public class Triangle
+        {
+            public Point2D A { get; }
+            public Point2D B { get; }
+            public Point2D C { get; }
+
+            private readonly IChecker checker;
+
+            public Triangle (Point2D a, Point2D b, Point2D c, IChecker checker)
+            {
+                A = a;
+                B = b;
+                C = c;
+                this.checker = checker;
+
+            }
+            public bool Contains(Point2D point)
+            {
+               return checker.Check(point, this);
+            }
+
+        }
+        public class TriangleCheker : IChecker
+        {
+            public bool Check (Point2D point, Triangle triangle)
+            {
+                double Area (Point2D p1, Point2D p2, Point2D p3)
+                {
+                    double a = p1.Distance(p2);
+                    double b = p2.Distance(p3);
+                    double c = p1.Distance(p3);
+                    double perimetr = (a + b + c) / 2;
+
+                    return Math.Sqrt(perimetr * (perimetr - a) * (perimetr - b) * (perimetr - c));
+                }
+                double AreaNoP = Area (triangle.A, triangle.B, triangle.C);
+
+                double AreaP1 = Area(point, triangle.B, triangle.C);
+                double AreaP2 = Area(triangle.A, point, triangle.C);
+                double AreaP3 = Area(triangle.A, triangle.B, point);
+
+                return Math.Abs(AreaNoP - (AreaP1 + AreaP2 + AreaP3)) < 1e-4;  //1 * 10^-7
+
+            }
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab1_c_;
+using static Lab1_c_.Objects;
+
+namespace Lab1_c_
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Point2D A = new Point2D(0.001, 0.01);
+            Point2D B = new Point2D(3.01, 2.01);
+            Point2D C = new Point2D(3.01, 0.01);
+            Point2D Point = new Point2D(1, 0.01);
+
+            TriangleCheker cheсker = new TriangleCheker();
+
+            Triangle triangle = new Triangle(A, B, C, cheсker);
+
+            bool IsInside = triangle.Contains(Point);
+
+            Console.WriteLine($"Точка принадлежит треугольнику: {IsInside}");
+
+        }
+    }
+}
+C: \Users\User\source\repos\Lab1_c#\
